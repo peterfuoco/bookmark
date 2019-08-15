e394aca0982d4a6671b4697b5f207f71c714c6da
Peter Fuoco <peterfuoco@Peters-MacBook-Pro.local>
2019-08-15 16:50:43 -0400

    made controller

---
+++ bookmark_api/Controllers/bookmarks.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bookmarks = express.Router();
+const Bookmark = require('../models/bookmarks');
+
+bookmarks.get('/', (req,res) => {
+    res.send('heyoooo')
+});
+
+
+
+module.exports = bookmarks;
+
+
+
+
