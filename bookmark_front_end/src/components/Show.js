d08f3e3a1fdb942840545feae799899c08fcb5df
Peter Fuoco <peterfuoco@Peters-MacBook-Pro.local>
2019-08-15 19:04:03 -0400

    Show component now shows website info on browser

---
+++ bookmark_front_end/src/components/Show.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class Show extends React.Component {
+    render () {
+        return (
+            <div className='show'>
+            <h1>Website List</h1>
+            
+            {this.props.bookmarks.map(bookmark => {
+                return (
+                    <div> 
+                   <h3>  {bookmark.title}</h3>
+                   <h3> {bookmark.url} </h3>
+                   </div>
+                )
+            })}
+            </div>
+        )
+    }
+
+
+} 
+
+export default Show
+
