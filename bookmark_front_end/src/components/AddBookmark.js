5ebb5a1b952dccd658d75cad39bcb844460191f7
Nick Ortiz <nickortiz@Nicks-MacBook-Pro.local>
2019-08-15 19:09:38 -0400

    added bookmarks

--- bookmark_front_end/src/components/AddBookmark.js
+++ bookmark_front_end/src/components/AddBookmark.js
@@ -1,23 +1,63 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class AddBookmark extends Component {
-    constructor(props) {
-    super(props)
-    this.state= {
-        bookmark: ''
-     }
-}
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      url: ''
+    };
+  }
 
+  handleChange(event) {
+    const { name, value } = event.currentTarget;
+    this.setState({
+      [name]: value
+    });
+  }
 
+  async handleSubmit(event) {
+    event.preventDefault();
+    const baseURL = this.props.baseURL;
+    const response = await Axios.post(`${baseURL}/bookmarks`, {
+      title: this.state.title,
+      url: this.state.url
+    });
+    this.setState({
+      title: '',
+      url: ''
+    });
+    this.props.handleAddBookmark(response.data);
+  }
 
-
-
-render() {
+  render() {
     return (
+      <div>
         <h1> Bookmark App</h1>
-
-    )}
-
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor='title' />
+          <input
+            type='text'
+            id='title'
+            name='title'
+            onChange={this.handleChange}
+            value={this.state.title}
+            placeholder='Website Title'
+          />
+          <label htmlFor='url' />
+          <input
+            type='text'
+            id='url'
+            name='url'
+            onChange={this.handleChange}
+            value={this.state.url}
+            placeholder='https://'
+          />
+          <input type='submit' value='Add Bookmark' />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default AddBookmark;
